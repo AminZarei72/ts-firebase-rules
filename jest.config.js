13e80a7a0deac94fa014855170c882ab95b633bd
amin_zarei <aminzarei.work@gmail.com>
2022-02-01 12:17:13 +0330

    init basics(using a blank boilerplate for generating node modules with some modifications ) ;

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    globals: {
+        "ts-jest": {
+            tsConfig: "tsconfig.json"
+        }
+    },
+    moduleFileExtensions: [
+        "ts",
+        "js"
+    ],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+    testMatch: [
+        "**/__tests__/e2e/**/*.(ts|js)"
+    ],
+    testEnvironment: "node"
+};
