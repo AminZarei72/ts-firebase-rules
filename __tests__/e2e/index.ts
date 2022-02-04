d3d7604b6b457c4278f27829bb6e64bf8d8f07ad
amin_zarei <aminzarei.work@gmail.com>
2022-02-04 20:25:19 +0330

    improve create-command tests by adding subfolders and make it optional(to fix duplication bugs)

--- __tests__/e2e/index.ts
+++ __tests__/e2e/index.ts
@@ -4,13 +4,38 @@ import * as configs from '../../src/configs'
 /* ======================================== */
 test('"create" works', async () => {
     expect(1 + 2).toBe(3)
-    const res = shell.exec('npx tsfr') 
+    const res = shell.exec('npx tsfr')
     expect(res.stdout.includes(configs.appName)).toBeTruthy()
 
     // const res2 = shell.exec('npx tsfr create')
-    const res2 = shell.exec('npx tsfr create --template todoList')
-    expect(res2.stdout.includes('creating')).toBeTruthy()
+    // const res2 = shell.exec('npx tsfr create --template todoList')
+    // expect(res2.stdout.includes('creating')).toBeTruthy()
+
+    const currentDir = `./__tests__/tmp/e2e1Files`
+    const userChoosedDir = `tsfrPrj`
+    /* ----------- */
+    shell.rm('-rf', currentDir)
+    // fs.mkdirSync(currentDir)
+    fs.ensureDirSync(currentDir)
+
+    await testCreate({ currentDir: `${currentDir}/prj1`, userChoosedDir })
+
 
 })
+/* ======================================== */
+async function testCreate(args: { currentDir: string, userChoosedDir: string }) {
+    console.log('user is initilizing its tsfr project...')
+    fs.mkdirSync(args.currentDir)
+    const { code } = shell.exec(`cd ${args.currentDir} && npx tsfr create ${args.userChoosedDir} --template todoList`)
+    if (code != 0) throw 'failed'
+    /* these will be made from scratch */
+    expect(fs.existsSync(`${args.currentDir}/tsfr.config.json`)).toBeTruthy()
+    /* these will get coppied from template folder */
+    // expect(fs.existsSync(`${args.currentDir}/${args.userChoosedDir}/index.ts`)).toBeTruthy()
 
+    expect(fs.readdirSync(`${args.currentDir}/${args.userChoosedDir}/`, {
+        withFileTypes: true
+    })).toMatchSnapshot('filesList')
 
+}
+/* ======================================== */
